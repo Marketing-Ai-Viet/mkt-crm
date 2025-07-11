208893e5efa7d16f74dd26bd89c418e934c24c23
Kiennt-Dev <kiennt1306.dev@gmail.com>
2025-07-11 11:06:26 +0800

    fix import module core

--- packages/twenty-server/src/engine/api/graphql/graphql-config/graphql-config.module.ts
+++ packages/twenty-server/src/engine/api/graphql/graphql-config/graphql-config.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 
 import { CoreEngineModule } from 'src/engine/core-modules/core-engine.module';
-import { MKTCoreModule } from 'src/mkt-core/mkt-core.module';
 @Module({
-  imports: [CoreEngineModule,MKTCoreModule],
+  imports: [CoreEngineModule],
   providers: [],
-  exports: [CoreEngineModule,MKTCoreModule],
+  exports: [CoreEngineModule],
 })
 export class GraphQLConfigModule {}
