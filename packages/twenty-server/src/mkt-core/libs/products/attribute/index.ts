24e16191b19d42957b67e470d448ec443dafdce5
Kin Kin <kiennt1306.dev@gmail.com>
2025-07-30 16:06:00 +0800

    Revert "Products Module"

--- packages/twenty-server/src/mkt-core/libs/products/attribute/index.ts
+++
@@ -1,5 +0,0 @@
-export { MktProductAttributeWorkspaceEntity } from './entity';
-export { PRODUCT_ATTRIBUTE_SEEDS_CONSTANT, ATTRIBUTE_SEED_IDS } from './constants';
-export { prefillProductAttributes } from './seed/data';
-export { productAttributesAllView } from './seed/view';
-export { SeedAttributeModuleCommand } from './seed/command';
--- packages/twenty-server/src/mkt-core/libs/products/variant/index.ts
+++
@@ -1,8 +0,0 @@
-export { MktProductVariantWorkspaceEntity } from './entity';
-export { prefillProductVariants } from './seed/data';
-export { productVariantsAllView } from './seed/view';
-export { SeedVariantModuleCommand } from './seed/command';
-export { PRODUCT_VARIANT_SEEDS_CONSTANT } from './constants';
-
-//internal
-export { PRODUCT_VARIANT_IDS } from './constants';
