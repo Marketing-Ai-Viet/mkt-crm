a008ef4380790b93404aefabcf1c36d3bffbb9f3
nghuyan304 <nghuyan304@gmail.com>
2025-08-18 15:35:12 +0700

    refactor: simplify MktCoreModule imports by removing unused modules
    - Removed VariantModule and ConfigurationModule from MktCoreModule imports to streamline the module structure.
    - Enhanced clarity and maintainability of the MktCoreModule.

--- packages/twenty-server/src/mkt-core/mkt-core.module.ts
+++ packages/twenty-server/src/mkt-core/mkt-core.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MktMiddlewareModule } from 'src/mkt-core/middlewares/mkt-middleware.module';
 import { MktOrderModule } from 'src/mkt-core/order/mkt-order.module';
-import { ConfigurationModule } from './configuration/configuration.module';
 import { InvoiceModule } from './invoice/invoice.module';
-import { VariantModule } from './variant/variant.module';
 
 @Module({
   imports: [
-    VariantModule, 
-    ConfigurationModule, 
     MktOrderModule,
     InvoiceModule,
     MktMiddlewareModule,
