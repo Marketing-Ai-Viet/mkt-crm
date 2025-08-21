47e3b0b719c02effcda1b4e9fb9fc96c363b453f
nghuyan304 <nghuyan304@gmail.com>
2025-08-21 15:14:52 +0700

    feat(mkt-core): add MktLicenseCreateOnePreQueryHook for license creation
    - Introduced MktLicenseCreateOnePreQueryHook to handle pre-query logic for creating licenses.
    - Implemented logic to generate license data based on mktOrderId if the name is not provided in the input.
    - Updated MktLicenseModule to include the new pre-query hook for enhanced functionality.

--- packages/twenty-server/src/mkt-core/license/mkt-license.module.ts
+++ packages/twenty-server/src/mkt-core/license/mkt-license.module.ts
@@ -1,10 +1,14 @@
 import { MiddlewareConsumer,Module,RequestMethod } from '@nestjs/common';
 
+import { MktLicenseCreateOnePreQueryHook } from 'src/mkt-core/license/mkt-license-create-one.pre-query-hook';
 import { MktLicenseMiddleware } from 'src/mkt-core/license/mkt-license.middleware';
 import { MktLicenseService } from 'src/mkt-core/license/mkt-license.service';
 
 @Module({
-  providers: [MktLicenseService],
+  providers: [
+    MktLicenseService,
+    MktLicenseCreateOnePreQueryHook,
+  ],
 })
 export class MktLicenseModule {
   configure(consumer: MiddlewareConsumer) {
