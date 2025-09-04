af5217354afc766ec63372da54d6fc1c1120e6ad
phuth <phuth@phanmemmkt.vn>
2025-09-04 08:28:07 +0700

    feature#111: feature - implement rbac

--- packages/twenty-server/src/mkt-core/mkt-core.module.ts
+++ packages/twenty-server/src/mkt-core/mkt-core.module.ts
@@ -4,14 +4,14 @@ import { MktInvoiceModule } from 'src/mkt-core/invoice/mkt-invoice.module';
 import { MktLicenseModule } from 'src/mkt-core/license/mkt-license.module';
 import { MktOrderModule } from 'src/mkt-core/order/mkt-order.module';
 import { MktRbacModule } from 'src/mkt-core/mkt-rbac/mkt-rbac.module';
+import { MktDepartmentModule } from 'src/mkt-core/mkt-department/mkt-department.module';
 
 @Module({
   imports: [
-    // Business modules first
     MktOrderModule,
     MktInvoiceModule,
     MktLicenseModule,
-
+    MktDepartmentModule,
     // RBAC module last - with proper dependency injection
     MktRbacModule.forRoot(),
   ],
