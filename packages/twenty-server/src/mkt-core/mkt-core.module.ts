9adbb2c071ea8ef67988c873279844b2bbba3bf6
Nguyen Tinh <tinhnq@phanmemmkt.vn>
2025-09-18 14:38:31 +0700

    create new user without any roles

--- packages/twenty-server/src/mkt-core/mkt-core.module.ts
+++ packages/twenty-server/src/mkt-core/mkt-core.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 
 import { MktInvoiceModule } from 'src/mkt-core/invoice/mkt-invoice.module';
 import { MktLicenseModule } from 'src/mkt-core/license/mkt-license.module';
-import { MktOrderModule } from 'src/mkt-core/order/mkt-order.module';
 import { MktDepartmentModule } from 'src/mkt-core/mkt-department/mkt-department.module';
+import { MktOrderModule } from 'src/mkt-core/order/mkt-order.module';
+import { PersonUserManagementModule } from 'src/mkt-core/user-management/person-user-management.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { MktDepartmentModule } from 'src/mkt-core/mkt-department/mkt-department.
     MktInvoiceModule,
     MktLicenseModule,
     MktDepartmentModule,
+    PersonUserManagementModule,
   ],
 })
 export class MktCoreModule {}
---
+++ packages/twenty-server/src/mkt-core/user-management/person-user-management.resolver.ts
@@ -0,0 +1,30 @@
+import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+
+import { UserAuthGuard } from 'src/engine/guards/user-auth.guard';
+import { WorkspaceAuthGuard } from 'src/engine/guards/workspace-auth.guard';
+import { TwentyORMGlobalManager } from 'src/engine/twenty-orm/twenty-orm-global.manager';
+import { CreateUserInput } from 'src/mkt-core/user-management/dto/create-user.input';
+import { UserOutput } from 'src/mkt-core/user-management/dto/user.output';
+import { PersonUserManagementService } from 'src/mkt-core/user-management/person-user-management.service';
+
+@UseGuards(UserAuthGuard, WorkspaceAuthGuard)
+@Resolver(() => UserOutput)
+export class PersonUserManagementResolver {
+  constructor(
+    private readonly personUserManagementService: PersonUserManagementService,
+    private readonly twentyORMGlobalManager: TwentyORMGlobalManager,
+  ) {}
+
+  @Mutation(() => UserOutput)
+  async createPersonUser(
+    @Args('workspaceId', { type: () => String }) workspaceId: string,
+    @Args('input', { type: () => CreateUserInput })
+    input: CreateUserInput,
+  ) {
+    return this.personUserManagementService.createPersonUser(
+      workspaceId,
+      input,
+    );
+  }
+}
