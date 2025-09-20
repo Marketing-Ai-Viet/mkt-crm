9217247e2cd5bfc8b319682cd3924cdd330bba4c
Nguyen Tinh <tinhnq@phanmemmkt.vn>
2025-09-20 11:42:58 +0700

    \2FA-authentication-with-email

--- packages/twenty-server/src/mkt-core/mkt-two-facetor-authentication/mkt-two-facetor-authentication.resolver.ts
+++ packages/twenty-server/src/mkt-core/mkt-two-facetor-authentication/mkt-two-facetor-authentication.resolver.ts
@@ -1,6 +1,7 @@
 import { Args, Field, Mutation, ObjectType, Resolver } from '@nestjs/graphql';
 
 import { AuthTokens } from 'src/engine/core-modules/auth/dto/token.entity';
+
 import { MtkTwoFacetorAuthGetOtpSendMailInput } from 'src/mkt-core/mkt-two-facetor-authentication/dto/mtkTwoFacetorAuthGetOtpSendMail.input';
 import { MtkTwoFacetorAuthSetOtpSendMailInput } from 'src/mkt-core/mkt-two-facetor-authentication/dto/mtkTwoFacetorAuthSetOtpSendMail.input';
 import { MktTwoFacetorAuthenticationService } from './mkt-two-facetor-authentication.service';
